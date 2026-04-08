7f575882db59b2053d6fc44c69392be040acb76e
jihadcodes <jihad.dev.pro@gmail.com>
2026-04-08 20:01:17 +0600

    md and lg device responsiveness for hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,28 +3,33 @@ import ArrowDown from "@/assets/icons/arrow-down.svg";
 import Image from "next/image";
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60">
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={memojiImage} className="size-[100px]" alt="Memoji" />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-500 size-2.5 rounded-full "></div>
-            <div className="text-sm font-medium">Available for new projects</div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
           </div>
         </div>
-        <h1 className="font-serif text-3xl text-center mt-8  tracking-wide">Building Exceptional User Experiences</h1>
+        <div className="max-w-lg justify-center mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8  tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
 
-        <p className="mt-4 text-center text-white/60">
-          I specialized in transforming designs into functional,
-          high-perfortmance web applications. Let's discuss your next project.
-        '
-        </p>
-        <div className="flex flex-col items-center mt-8  ">
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialized in transforming designs into functional,
+            high-perfortmance web applications. Let's discuss your next project.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4 ">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl ">
             <span className="font-semibold ">Explore My Work</span>
             <ArrowDown className="size-4" />
           </button>
-          <button className="mt-4 inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl ">
+          <button className=" inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl ">
             <span className="font-semibold  ">Let's Connect</span>
           </button>
         </div>
