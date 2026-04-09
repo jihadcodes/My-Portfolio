de00f41f49850c2d761e9fad1c702ad1661f1631
jihadcodes <jihad.dev.pro@gmail.com>
2026-04-09 11:11:54 +0600

    projec section creating with mobile first responsive

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,6 +2,8 @@ import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
+import CheckCirleIcon from "@/assets/icons/check-circle.svg"
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
 
 const portfolioProjects = [
   {
@@ -52,20 +54,30 @@ export const ProjectsSection = () => {
       <p className="text-center text-white/60 mt-4 ">See how I transformed concepts into engaging digital experiences.</p>
       <div className="flex flex-col mt-10 ">
         {portfolioProjects.map((project, index) => (
-          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline  after:-outline-offset-2 after:rounded-3xl after:outline-white/20 ">
-            
-             <div>
+          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline  after:-outline-offset-2 after:rounded-3xl after:outline-white/20 p-8 ">
+
+            <div className="flex  ">
+               <div className=" bg-gradient-to-r from-emerald-300 to-sky-400  inline-flex gap-2  font-bold uppercase tracking-widest test-sm text-transparent bg-clip-text   ">
               <span>{project.company}</span>
               <span>{project.year}</span>
              </div>
-             <h3>{project.title}</h3>
-             <ul>
+            </div>
+             <h3 className="font-serif text-2xl mt-2 ">{project.title}</h3>
+             <hr className="border-t-2 border-white/5 mt-4 "/>
+             <ul  className="flex flex-col gap-4 mt-4 ">
                {project.results.map((result, index) => (
-                 <li key={index}>{result.title}</li>
+                 <li key={index} className="flex text-sm text-white/50  gap-2">
+                  <CheckCirleIcon className="size-5" />
+                  <span>{result.title}</span>
+                 </li>
                ))}
              </ul>
              <a href={project.link} target="_blank" rel="noopener noreferrer">
-               <button>View Live Site</button>
+               <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold  inline-flex items-center justify-center gap-2 mt-8 ">
+                <span>View Live Site</span>
+                <ArrowUpRightIcon className="size-4" />
+
+               </button>
              </a>
              <Image src={project.image} alt={project.title} />
           </div>
