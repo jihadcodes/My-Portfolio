243bc674733ed49a99e8989c64b45bcd9008fb86
jihadcodes <jihad.dev.pro@gmail.com>
2026-04-23 00:54:33 +0600

    tape section done in medium devices

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -5,6 +5,7 @@ const words = [
   "Accessible",
   "Secure",
   "Interactive",
+  "Responsive",
   "Scalable",
   "User Frindly",
   "Maintainable",
@@ -15,7 +16,7 @@ const words = [
 
 export const TapeSection = () => {
   return (
-    <div className=" py-16">
+    <div className=" py-16 lg:py-24 ">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400  overflow-x-clip -rotate-3 -mx-1 ">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex flex-none  gap-4 py-3  ">
